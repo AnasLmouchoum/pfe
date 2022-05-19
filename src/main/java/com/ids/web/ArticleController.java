97070f4298bca7e4b7041642d38fb28bb9a3bd62
Zouhir HANAFI <zouhir.hanafi@gmail.com>
2022-05-19 18:14:54 +0100

    suppression commentaire

--- src/main/java/com/ids/web/ArticleController.java
+++ src/main/java/com/ids/web/ArticleController.java
@@ -9,17 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ids.data.web.AbstractCrudController;
 import com.ids.entity.Article;
 
-/*
- * git clone -b v_omar https://github.com/innovds/contact-ms.git
- * git remote add r_omar https://github.com/innovds/contact-ms.git 
- *git add .
- *
- * git commit -m " un comm"
- * git push -u r_omar v_omar
- * et apres git push 
- * sinon 
- * git push -f r_omar v_omar
- * */
 @RestController
 @RequestMapping("/api/v1/articles")
 @CrossOrigin("*")
